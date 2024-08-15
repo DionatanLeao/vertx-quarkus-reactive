e83dc3d68e77bcb35fc354daa4e471973975aa2e
DionatanLeao <dionatan.ti@outlook.com>
2024-08-15 12:28:08 -0300

    feat/Lesson: Quarkus Reactive - RESTEasy and Hibernate Reactive with Panache Part 2

--- src/main/java/com/udemy/quarkus/UserResource.java
+++ src/main/java/com/udemy/quarkus/UserResource.java
@@ -1,15 +1,15 @@
 package com.udemy.quarkus;
 
+import io.quarkus.hibernate.reactive.panache.Panache;
 import io.quarkus.panache.common.Sort;
 import io.smallrye.mutiny.Uni;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.net.URI;
 import java.util.List;
 
 @Path("/users")
@@ -23,5 +23,21 @@ public class UserResource {
     public Uni<List<User>> get() {
         LOG.info("Get all users...");
         return User.listAll(Sort.by("id"));
+    }
+
+    @GET
+    @Path("/{id}")
+    public Uni<User> getById(Long id) {
+        LOG.info("Get by id: {}", id);
+        return User.findById(id);
+    }
+
+    @POST
+    public void create(User user) {
+        LOG.info("Create: {}", user);
+        Panache.<User>withTransaction(user::persist)
+        .onItem().transform(insertedUser ->
+            Response.created(URI.create("/users/" + insertedUser.id)).build()
+        );
     }
 }
