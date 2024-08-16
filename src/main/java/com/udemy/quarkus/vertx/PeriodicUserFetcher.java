53d3a240810e661438947842b08528d50595aa6b
DionatanLeao <dionatan.ti@outlook.com>
2024-08-15 21:50:32 -0300

    feat/Lesson: Quarkus Reactive - Vert.x EventBus

--- src/main/java/com/udemy/quarkus/vertx/PeriodicUserFetcher.java
+++ src/main/java/com/udemy/quarkus/vertx/PeriodicUserFetcher.java
@@ -2,6 +2,8 @@ package com.udemy.quarkus.vertx;
 
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.mutiny.ext.web.client.WebClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,15 +13,26 @@ import java.time.Duration;
 @ApplicationScoped
 public class PeriodicUserFetcher extends AbstractVerticle {
     private static final Logger LOG = LoggerFactory.getLogger(PeriodicUserFetcher.class);
+    public static final String ADDRESS = PeriodicUserFetcher.class.getName();
 
     @Override
     public Uni<Void> asyncStart() {
+        var client = WebClient.create(vertx,
+                new WebClientOptions().setDefaultHost("localhost").setDefaultPort(8080)
+        );
+
         vertx.periodicStream(Duration.ofSeconds(5).toMillis())
-                .toMulti()
-                .subscribe()
-                .with(item -> {
-                    LOG.info("Hello from PeriodicUserFetcher!");
-                });
+            .toMulti()
+            .subscribe()
+            .with(item -> {
+                LOG.info("Fetch all users!");
+                client.get("/users").send()
+                    .subscribe().with( result -> {
+                        var body = result.bodyAsJsonArray();
+                        LOG.info("All users from http response: {}", body);
+                        vertx.eventBus().publish(ADDRESS, body);
+                    });
+        });
         return Uni.createFrom().voidItem();
     }
 }
